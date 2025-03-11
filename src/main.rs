778628fcf826095805c1dcc94d9472ff8d9e152c
Valeriia Morkhat <mssolipsismus@gmail.com>
2025-03-11 20:27:45 +0100

    add ugly router

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use std::fs;
 use axum::{
-    routing::get,
-    Router,
+    http::{header, StatusCode},
     response::{IntoResponse, Response},
-    http::header,
+    routing::{get, post},
+    Json, Router,
 };
+use serde::{Deserialize, Serialize};
+use std::fs;
 use tokio::net::TcpListener;
+use tracing_subscriber;
+use tracing;
 
 async fn serve_gif() -> Response {
     let data = fs::read(r"C:\Users\Valeriia Morkhat\IdeaProjects\meetup\src\files\sticker.gif").expect("Failed to read sticker.gif");
@@ -15,10 +18,35 @@ async fn serve_gif() -> Response {
     ).into_response()
 }
 
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
+}
+
+async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    (StatusCode::CREATED, Json(user)).into_response()
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(serve_gif));
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(serve_gif))
+        .route("/users", post(create_user));
 
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
